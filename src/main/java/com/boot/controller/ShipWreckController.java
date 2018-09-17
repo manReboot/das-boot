7815a38e0a466f4be28d77df9d1fe035f40b38f8
man.reboot@gmail.com <man.reboot@gmail.com>
2018-09-18 02:23:37 +0800

    added the mock function

--- src/main/java/com/boot/controller/ShipWreckController.java
+++ src/main/java/com/boot/controller/ShipWreckController.java
@@ -1,9 +1,14 @@
 package com.boot.controller;
 
 import com.boot.model.Shipwreck;
+import com.boot.repository.ShipWreckRepo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by 607880661 on 17/09/2018.
@@ -12,27 +17,44 @@ import java.util.List;
 @RequestMapping("/api/v1/")
 public class ShipWreckController {
 
+    @Autowired
+    private ShipWreckRepo shipWreckRepo;
+
     @RequestMapping(value ="shipwrecks", method = RequestMethod.GET)
     public List<?> getShipList()
     {
-        return ShipwreckStub.list();
+        return shipWreckRepo.findAll();
     }
 
     @RequestMapping(value="/shipwrecks/{id}", method=RequestMethod.GET)
     public Shipwreck getShipwreck(@PathVariable Long id)
     {
-        return ShipwreckStub.get(id);
+        Optional<Shipwreck> existingShipwreck =  shipWreckRepo.findById(id);
+        return existingShipwreck.get();
     }
 
     @RequestMapping(value="/shipwrecks", method=RequestMethod.POST)
     public Shipwreck create(@RequestBody Shipwreck shipwreck)
     {
-        return ShipwreckStub.create(shipwreck);
+        return shipWreckRepo.saveAndFlush(shipwreck);
+    }
+
+    @RequestMapping(value="/shipwrecks/{id}", method=RequestMethod.PUT)
+    public Shipwreck update(@PathVariable Long id, @RequestBody Shipwreck shipwreck)
+    {
+        Optional<Shipwreck> existingShipwreckOption =  shipWreckRepo.findById(id);
+        Shipwreck existingShipWreck = existingShipwreckOption.get();
+        BeanUtils.copyProperties(shipwreck,existingShipWreck);
+        return shipWreckRepo.saveAndFlush(existingShipWreck);
+
     }
 
     @RequestMapping(value="/shipwrecks/{id}", method=RequestMethod.DELETE)
     public Shipwreck delete(@PathVariable Long id)
     {
-        return ShipwreckStub.delete(id);
+        Optional<Shipwreck> existingShipwreckOption =  shipWreckRepo.findById(id);
+        Shipwreck existingShipWreck = existingShipwreckOption.get();
+        shipWreckRepo.delete(existingShipWreck);
+        return existingShipWreck;
     }
 }
